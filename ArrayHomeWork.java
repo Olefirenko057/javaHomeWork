bdfd3159401b6c7a497f96ac282da892dd66edfa
Olefirenko057 <117396653+Olefirenko057@users.noreply.github.com>
2023-06-12 22:54:33 +0200

    Add files via upload

---
+++ ArrayHomeWork.java
@@ -0,0 +1,16 @@
+package Homework11;
+
+public class ArrayHomeWork {
+    public static void main(String[] args) {
+        String[] weekDays = new String[7];
+        weekDays[0] = "Monday";
+        weekDays[1] = "Tuesday";
+        weekDays[2] = "Wednesday";
+        weekDays[3] = "Thursday";
+        weekDays[4] = "Friday";
+        weekDays[5] = "Saturday";
+        weekDays[6] = "Sunday";
+
+        System.out.println(weekDays[weekDays.length - 1]);
+     }
+}
